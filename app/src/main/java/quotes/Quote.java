e1bf740f28465ef4ce0356105bc7cd12ce248a91
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-10-05 19:23:44 +0300

    lab8

---
+++ app/src/main/java/quotes/Quote.java
@@ -0,0 +1,32 @@
+package quotes;
+
+
+public class Quote {
+    private String author;
+    private String text;
+    public Quote(){
+
+    }
+
+    public Quote(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
+
